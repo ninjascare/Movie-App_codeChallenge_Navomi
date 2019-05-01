7fcf50cbe8cdf2b480ec34e3dfebd28e0a5e781c
ninjascare <kevin76475@gmail.com>
2019-05-01 12:31:35 -0400

    finished my backend

--- controllers/movie.js
+++ controllers/movie.js
@@ -8,6 +8,27 @@ const moviesController = {
   show: (req, res) => {
     const movieId = req.params.movieId;
     Movies.findById(movieId).then(movie => res.send(movie));
+  },
+
+  create: (req, res) => {
+    Movies.create(req.body).then(newMovie => {
+      res.send(newMovie);
+    });
+  },
+
+  update: (req, res) => {
+    const movieId = req.params.movieId;
+    Movies.findByIdAndUpdate(movieId, req.body).then(updatedMovie => {
+      updatedMovie.save();
+      res.send(updatedMovie);
+    });
+  },
+
+  delete: (req, res) => {
+    const movieId = req.params.movieId;
+    Movies.findByIdAndRemove(movieId).then(() => {
+      res.send(200);
+    });
   }
 };
 
