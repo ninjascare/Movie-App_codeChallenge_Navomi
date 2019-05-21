a382816002fc90b173e89a2aea0ef3ba0404cfcc
Kevin Rosales <kevinrosales@Kevins-MacBook-Pro.local>
2019-05-21 11:19:46 -0400

    added a mocha script

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const routes = require("./routes/index");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static(__dirname + "/client/build/"));
