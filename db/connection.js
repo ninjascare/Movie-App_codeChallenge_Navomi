b96db58f53d174e1c94e8e9edd0bf84a8b7f5cd0
ninjascare <kevin76475@gmail.com>
2019-06-07 16:44:55 -0400

    yeh

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
 
-mongoose.connection.once("open", () => {
-  console.log(`Mongoose has connected to MongoDB`);
-});
-
-mongoose.connection.on("error", error => {
-  console.error(`MongoDB connection error!!! ${error}`);
+mongoose.connection.on("error", function(err) {
+  console.error("MongoDB connection error: " + err);
   process.exit(-1);
+});
+mongoose.connection.once("open", function() {
+  console.log("Mongoose has connected to MongoDB!");
 });
 
 module.exports = mongoose;
